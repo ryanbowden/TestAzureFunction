e85d39044ff44e8d78ddf87d12ca1f7523687550
Ryan Bowden <ryan.bowden@esendex.com>
2019-07-08 12:32:57 +0100

    started again fresh

---
+++ MyFunction/MyFunction.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace MyFunction.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.False(false);
+        }
+    }
+}
